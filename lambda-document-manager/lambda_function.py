ef0ef515cd374709d1cc22225db967da41d48e90
John Park <kyopark2014@gmail.com>
2025-03-18 10:58:52 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -637,23 +637,28 @@ def get_contextual_text(whole_text, splitted_text):
         ('human', contextual_template)
     ])
 
-    llm = get_model()
-        
-    contexual_chain = contextual_prompt | llm
+    contextual_text = []
+    for i, text in enumerate(splitted_text):
+        print(f"--> {i}: {text}")
+        llm = get_model()
             
-    response = contexual_chain.invoke(
-        {
-            "WHOLE_DOCUMENT": whole_text,
-            "CHUNK_CONTENT": splitted_text
-        }
-    )    
-    # print('--> contexual rext: ', response)
-    output = response.content
-    contextual_text = output[output.find('<result>')+8:output.find('</result>')]
+        contexual_chain = contextual_prompt | llm
+                
+        response = contexual_chain.invoke(
+            {
+                "WHOLE_DOCUMENT": whole_text,
+                "CHUNK_CONTENT": text
+            }
+        )    
+        # print('--> contexual rext: ', response)
+        output = response.content
+        result = output[output.find('<result>')+8:output.find('</result>')]
         
-    # print(f"--> whole_text: {whole_text}")
-    print(f"--> original_chunk: {splitted_text}")
-    print(f"--> contextual_text: {contextual_text}")
+        # print(f"--> whole_text: {whole_text}")
+        print(f"--> {i}, original_chunk: {splitted_text}")
+        print(f"--> {i}, contextual_text: {result}")
+
+        contextual_text.append(result)
         
     return contextual_text
 
@@ -1195,6 +1200,11 @@ def load_document(file_type, key):
 
             # extract page images
             if enablePageImageExraction=='Enable': 
+                contexual_text = []
+                if contextual_embedding == 'Enable':
+                    print('start contextual embedding for images.')
+                    contexual_text = get_contextual_text(contents, texts)
+
                 for i, page in enumerate(pages):
                     print('page: ', page)
                     
@@ -1211,16 +1221,17 @@ def load_document(file_type, key):
                             print(f"page[{i}] -> (used) width[{j}]: {bbox[2]-bbox[0]}, height[{j}]: {bbox[3]-bbox[1]}")                    
                         print(f"page[{i}] -> (image) width[{j}]: {info['width']}, height[{j}]: {info['height']}")
                         
-                    print(f"nImages[{i}]: {nImages[i]}")  # number of XObjects
+                    print(f"nImages[{i}]: {nImages[i]}")  # number of XObjects                    
+
                     if ocr=="Enable" or nImages[i]>=4 or \
                         (nImages[i]>=1 and (width==0 and height==0)) or \
                         (nImages[i]>=1 and (width>=100 or height>=100)):
 
-                        contexual_text = ""
-                        if contextual_embedding == 'Enable':
-                            print(f"texts[{i}]: {texts[i]}")
-                            if texts[i]:
-                                contexual_text = get_contextual_text(contents, texts[i])
+                        # contexual_text = ""
+                        # if contextual_embedding == 'Enable':
+                        #     print(f"texts[{i}]: {texts[i]}")
+                        #     if texts[i]:
+                        #         contexual_text = get_contextual_text(contents, texts[i])
 
                         # save current pdf page to image 
                         pixmap = page.get_pixmap(dpi=200)  # dpi=300
@@ -1241,8 +1252,8 @@ def load_document(file_type, key):
                         print('fname: ', fname)          
 
                         encoded_contexual_text = ""  # s3 meta only allows ASCII format
-                        if contexual_text:
-                            encoded_contexual_text = contexual_text.encode('ascii', 'ignore').decode('ascii')
+                        if contextual_embedding=='Enable' and contexual_text[i]:
+                            encoded_contexual_text = contexual_text[i].encode('ascii', 'ignore').decode('ascii')
                             print('encoded_contexual_text: ', encoded_contexual_text)
 
                         response = s3_client.put_object(
