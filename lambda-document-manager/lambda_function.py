34e1d97389ccf39fdba5cfcb4f791858e502c683
John Park <kyopark2014@gmail.com>
2025-03-20 00:06:33 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -814,7 +814,7 @@ def get_contextual_docs_using_parallel_processing(whole_doc, splitted_docs):
             parent_conn, child_conn = Pipe()
             parent_connections.append(parent_conn)
                 
-            process = Process(target=get_contextual_doc, args=(child_conn, whole_doc, splitted_docs[i], selected_model))
+            process = Process(target=get_contextual_doc, args=(child_conn, whole_doc, splitted_docs[index], selected_model))
             processes.append(process)
 
             selected_model = selected_model + 1
@@ -1411,7 +1411,7 @@ def extract_page_images_using_parallel_processing(key, pages, nImages, contents,
             parent_conn, child_conn = Pipe()
             parent_connections.append(parent_conn)
                 
-            process = Process(target=extract_page_image, args=(child_conn, key, pages[i], i, nImages, contents, texts[i], selected_model))
+            process = Process(target=extract_page_image, args=(child_conn, key, pages[index], index, nImages, contents, texts[index], selected_model))
             processes.append(process)
 
             selected_model = selected_model + 1
